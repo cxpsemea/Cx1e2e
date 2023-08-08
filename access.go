f713c187880e4809300c5546e00e443dea1b941f
michael kubiaczyk <michael.kubiaczyk@checkmarx.com>
2023-08-08 14:45:40 +0200

    updating test cases, adding RUD

--- access.go
+++ access.go
@@ -36,7 +36,7 @@ func AccessTestsCreate(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, 
 	}
 }
 
-func CreateAccessAssignment(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, t *AccessAssignmentCRUD) error {
+func prepareAccessAssignment(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, t *AccessAssignmentCRUD) (Cx1ClientGo.AccessAssignment, error) {
 	access := Cx1ClientGo.AccessAssignment{
 		TenantID:     cx1client.GetTenantID(),
 		EntityType:   t.EntityType,
@@ -47,19 +47,19 @@ func CreateAccessAssignment(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Log
 	case "user":
 		user, err := cx1client.GetUserByUserName(t.EntityName)
 		if err != nil {
-			return fmt.Errorf("failed to retrieve user with username %v: %s", t.EntityName, err)
+			return access, fmt.Errorf("failed to retrieve user with username %v: %s", t.EntityName, err)
 		}
 		access.EntityName = user.UserName
 		access.EntityID = user.UserID
 	case "group":
 		group, err := cx1client.GetGroupByName(t.EntityName)
 		if err != nil {
-			return fmt.Errorf("failed to retrieve group named %v: %s", t.EntityName, err)
+			return access, fmt.Errorf("failed to retrieve group named %v: %s", t.EntityName, err)
 		}
 		access.EntityName = group.Name
 		access.EntityID = group.GroupID
 	default:
-		return fmt.Errorf("unknown entitytype %v used for access assignment, options are: user, group", t.EntityType)
+		return access, fmt.Errorf("unknown entitytype %v used for access assignment, options are: user, group", t.EntityType)
 	}
 
 	switch t.ResourceType {
@@ -69,14 +69,14 @@ func CreateAccessAssignment(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Log
 	case "application":
 		app, err := cx1client.GetApplicationByName(t.ResourceName)
 		if err != nil {
-			return fmt.Errorf("failed to retrieve application named %v: %s", t.ResourceName, err)
+			return access, fmt.Errorf("failed to retrieve application named %v: %s", t.ResourceName, err)
 		}
 		access.ResourceName = app.Name
 		access.ResourceID = app.ApplicationID
 	case "project":
 		project, err := cx1client.GetProjectByName(t.ResourceName)
 		if err != nil {
-			return fmt.Errorf("failed to retrieve application named %v: %s", t.ResourceName, err)
+			return access, fmt.Errorf("failed to retrieve application named %v: %s", t.ResourceName, err)
 		}
 		access.ResourceName = project.Name
 		access.ResourceID = project.ProjectID
@@ -84,16 +84,21 @@ func CreateAccessAssignment(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Log
 
 	access.EntityRoles = t.Roles
 
-	err := cx1client.AddAccessAssignment(access)
+	return access, nil
+}
+
+func AccessTestCreate(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, testname string, t *AccessAssignmentCRUD) error {
+	access, err := prepareAccessAssignment(cx1client, logger, t)
+	if err != nil {
+		return err
+	}
+
+	err = cx1client.AddAccessAssignment(access)
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func AccessTestCreate(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, testname string, t *AccessAssignmentCRUD) error {
-	return CreateAccessAssignment(cx1client, logger, t)
 }
 
 func AccessTestsRead(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, testname string, accessAssignments *[]AccessAssignmentCRUD) {
@@ -117,6 +122,28 @@ func AccessTestsRead(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, te
 }
 
 func AccessTestRead(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, testname string, t *AccessAssignmentCRUD) error {
+	access, err := prepareAccessAssignment(cx1client, logger, t)
+	if err != nil {
+		return err
+	}
+
+	assignment, err := cx1client.GetAccessAssignmentByID(access.EntityID, access.ResourceID)
+	if err != nil {
+		return fmt.Errorf("no assignment matching %v", t.String())
+	}
+
+	for _, rr := range t.Roles {
+		hasrole := false
+		for _, ar := range assignment.EntityRoles {
+			if rr == ar {
+				hasrole = true
+				break
+			}
+		}
+		if !hasrole {
+			return fmt.Errorf("expected %v %v to have role %v on %v %v but it's not there", t.EntityType, t.EntityName, rr, t.ResourceType, t.ResourceName)
+		}
+	}
 
 	return nil
 }
@@ -142,6 +169,21 @@ func AccessTestsUpdate(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, 
 }
 
 func AccessTestUpdate(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, testname string, t *AccessAssignmentCRUD) error {
+	access, err := prepareAccessAssignment(cx1client, logger, t)
+	if err != nil {
+		return err
+	}
+
+	// TODO: update this once role-assignments are more granular
+	err = cx1client.DeleteAccessAssignmentByID(access.EntityID, access.ResourceID)
+	if err != nil {
+		return fmt.Errorf("failed to remove existing %v %v access to %v %v", t.EntityType, t.EntityName, t.ResourceType, t.ResourceName)
+	}
+
+	err = cx1client.AddAccessAssignment(access)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -167,6 +209,16 @@ func AccessTestsDelete(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, 
 }
 
 func AccessTestDelete(cx1client *Cx1ClientGo.Cx1Client, logger *logrus.Logger, testname string, t *AccessAssignmentCRUD) error {
+	access, err := prepareAccessAssignment(cx1client, logger, t)
+	if err != nil {
+		return err
+	}
+
+	// TODO: update this once role-assignments are more granular
+	err = cx1client.DeleteAccessAssignmentByID(access.EntityID, access.ResourceID)
+	if err != nil {
+		return fmt.Errorf("failed to remove existing %v %v access to %v %v", t.EntityType, t.EntityName, t.ResourceType, t.ResourceName)
+	}
 
 	return nil
 }
