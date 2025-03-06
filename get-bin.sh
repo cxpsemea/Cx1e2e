0fe1eca6acfeb0a7d85012f9410e6556562abbfa
michael kubiaczyk <michael.kubiaczyk@checkmarx.com>
2025-03-06 09:38:08 +0100

    adding a get-bin script to facilitate downloading latest binary

---
+++ get-bin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+URL=$(curl -s https://api.github.com/repos/cxpsemea/cx1e2e/releases/latest | grep "https.*cx1e2e\"" | sed "s/^.* \"//g" | sed "s/\".*$//g")
+echo URL is $URL
+wget $URL -O cx1e2e-bin
+chmod +x ./cx1e2e-bin
