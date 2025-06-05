6223a0326ab78b7fd31289ccf7ea0c205be0c8de
michael kubiaczyk <NOREPLY_EMAIL>
2025-06-05 07:24:17 +0200

    fix to use /bin/sh instead of /bin/bash in get-bin.sh

--- get-bin.sh
+++ get-bin.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 URL=$(curl -s https://api.github.com/repos/cxpsemea/cx1e2e/releases/latest | grep "https.*cx1e2e\"" | sed "s/^.* \"//g" | sed "s/\".*$//g")
 echo URL is $URL
 wget $URL -O cx1e2e-bin
